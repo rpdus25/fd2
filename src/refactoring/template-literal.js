7a914b809a12564d23676be21c86442de9c5ad44
Genji(김수호) <suho.kim2@gmail.com>
2017-06-10 18:17:29 +0900

    delete

--- src/refactoring/template-literal.js
+++
@@ -1,5 +0,0 @@
-var date = new Date();
-var todayES3 = date.getFullYear()+'년 '+(date.getMonth() + 1)+'월 '+date.getDate()+'일 '+date.getHours()+':'+(date.getMinutes() < 10 ? '0'+date.getMinutes() : date.getMinutes());
-var todayES6 = '';
-console.log(todayES3);
-console.log(todayES6);
